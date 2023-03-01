9eab93925f40571e0baf6558416c39cc06c83b16
Ioachim Lihor <ioachim.lihor@radcom.com>
2023-03-01 22:52:28 +0200

    fix(grid): visualMap
    Signed-off-by: Ioachim Lihor <ioachim.lihor@radcom.com>

--- pyecharts/charts/composite_charts/grid.py
+++ pyecharts/charts/composite_charts/grid.py
@@ -46,7 +46,9 @@ class Grid(Base):
         # visualMap 配置添加
         visual_map = chart.options.get("visualMap")
 
-        if "visualMap" not in self.options.keys():
+        if (("visualMap" not in self.options.keys() or self.options.get("visualMap") is None) 
+            and visual_map is not None
+        ):
             self.options.update(visualMap=self.options.get("visualMap"))
         elif visual_map is not None:
             if isinstance(self.options.get("visualMap"), opts.VisualMapOpts):
